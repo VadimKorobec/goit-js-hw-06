3b21c7c7ae2f782bc6702f0209f0f7baea6eaf85
VadimKorobec <_s_e_t__@ukr.net>
2022-10-22 17:16:59 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const navItemEl = document.createElement('li');
+navItemEl.classList.add('item');
+navItemEl.textContent = ' NEW';
+
+const listEl = document.querySelector('#ingredients');
+listEl.appendChild(navItemEl);
+
+console.log(listEl);
