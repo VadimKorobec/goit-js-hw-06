9b7f5aec2046f86ffac1873c7eb4a91364fdc484
VadimKorobec <_s_e_t__@ukr.net>
2022-10-23 20:08:33 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,37 +1,37 @@
-// const input = document.querySelector('#validation-input');
+const input = document.querySelector('#validation-input');
 
-// input.addEventListener('blur', onChangeFocus);
+input.addEventListener('blur', onChangeFocus);
 
-// function onChangeFocus(event) {
-//   console.log(event);
-//   if (event.currentTarget.value.length == input.getAttribute('data-length')) {
-//     input.classList.add('valid');
-//   } else {
-//     input.classList.remove('valid');
-//     input.classList.add('invalid');
+function onChangeFocus(event) {
+  console.log(event);
+  if (event.currentTarget.value.length === Number(input.dataset.length)) {
+    input.classList.add('valid');
+  } else {
+    input.classList.remove('valid');
+    input.classList.add('invalid');
+  }
+}
+
+// const inputVal = document.getElementById('validation-input');
+// const totalLenght = inputVal.getAttribute('data-length');
+// const intTotallenght = parseInt(totalLenght, 10);
+// inputVal.addEventListener(
+//   'focus',
+//   function (event) {
+//     event.target.style.outline = 'none';
+//   },
+//   true
+// );
+// inputVal.addEventListener('blur', () => {
+//   if (inputVal.value.length === intTotallenght) {
+//     inputVal.classList.add('valid');
+//     inputVal.classList.remove('invalid');
 //   }
-// }
-
-const inputVal = document.getElementById('validation-input');
-const totalLenght = inputVal.getAttribute('data-length');
-const intTotallenght = parseInt(totalLenght, 10);
-inputVal.addEventListener(
-  'focus',
-  function (event) {
-    event.target.style.outline = 'none';
-  },
-  true
-);
-inputVal.addEventListener('blur', () => {
-  if (inputVal.value.length === intTotallenght) {
-    inputVal.classList.add('valid');
-    inputVal.classList.remove('invalid');
-  }
-  if (inputVal.value.length === 0) {
-    inputVal.classList.remove('valid');
-    inputVal.classList.remove('invalid');
-  }
-  if (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
-    inputVal.classList.add('invalid');
-  }
-});
+//   if (inputVal.value.length === 0) {
+//     inputVal.classList.remove('valid');
+//     inputVal.classList.remove('invalid');
+//   }
+//   if (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
+//     inputVal.classList.add('invalid');
+//   }
+// });
