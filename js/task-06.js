560d44042c3ef90687ceebd745b361dce868a69d
VadimKorobec <_s_e_t__@ukr.net>
2022-10-23 21:01:24 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -15,17 +15,16 @@
 const inputVal = document.getElementById('validation-input');
 const totalLenght = inputVal.getAttribute('data-length');
 const intTotallenght = parseInt(totalLenght, 10);
-// 
+
 inputVal.addEventListener('blur', () => {
   if (inputVal.value.length === intTotallenght) {
     inputVal.classList.add('valid');
     inputVal.classList.remove('invalid');
-  }
-  else if (inputVal.value.length === 0) {
+  } else if (inputVal.value.length === 0) {
     inputVal.classList.remove('valid');
     inputVal.classList.remove('invalid');
-  }
-  else (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
+  } else {
+    inputVal.value.length !== intTotallenght && inputVal.value.length !== 0;
     inputVal.classList.add('invalid');
   }
 });
