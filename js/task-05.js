eb97dc6c74bc8fb191c15a3400b476478e6d79e0
VadimKorobec <_s_e_t__@ukr.net>
2022-10-23 18:55:23 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,18 +1,21 @@
+// const input = document.querySelector('#name-input');
+// const span = document.querySelectorAll('#name-output');
+
+// console.log(span);
+
+// input.addEventListener('input', onInputChange);
+
+// function onInputChange(event) {
+//   span.textContent = event.currentTarget.value;
+// }
+
 const input = document.querySelector('#name-input');
-const span = document.querySelectorAll('#name-output');
+const span = document.getElementById('#name-output');
 
-input.addEventListener('input', onInputChange);
-
-function onInputChange(event) {
-  span.textContent = event.currentTarget.value;
-}
-
-// const input = document.getElementById(`name-input`);
-// const nameOutput = document.getElementById(`name-output`);
-// input.oninput = function () {
-//   if (input.value === '') {
-//     nameOutput.innerHTML = 'Anonymous';
-//   } else {
-//     nameOutput.innerHTML = input.value;
-//   }
-// };
+input.oninput = function () {
+  if (input.value === '') {
+    span.innerHTML = 'Anonymous';
+  } else {
+    span.innerHTML = input.value;
+  }
+};
