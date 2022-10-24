3e53b64192bff949bcc4b1cac6fbcf568dd65f2e
VadimKorobec <_s_e_t__@ukr.net>
2022-10-24 16:12:51 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,21 +1,13 @@
 const button = document.querySelector('.change-color');
 const color = document.querySelector('.color');
 const body = document.querySelector('body');
-body.classList.add('js-backgroundcolor');
-console.log(body);
-
-// button.addEventListener('click', () => {
-//   let hexColor = getRandomHexColor();
-//   button.style.backgroundColor = getRandomHexColor();
-//   color.textContent = getRandomHexColor();
-// });
 
 button.addEventListener('click', onClickMe);
 
 function onClickMe(evt) {
-  console.log(evt);
   let hexColor = getRandomHexColor();
-  body.classList.toggle('js-backgroundcolor') = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
 }
 
 function getRandomHexColor() {
