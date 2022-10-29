9d6d5d320a52f2f280d3f435e4f3cc91107fa91c
VadimKorobec <_s_e_t__@ukr.net>
2022-10-29 15:31:30 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,25 +1,25 @@
-// const input = document.querySelector('#name-input');
-// const span = document.querySelector('#name-output');
+const input = document.querySelector('#name-input');
+const span = document.querySelector('#name-output');
 
-// input.addEventListener('input', onInputChange);
-
-// function onInputChange(event) {
-//   span.textContent = event.currentTarget.value;
-//   if (input.value === '') {
-//     span.textContent = 'Anonymous';
-//   }
-// }
-
-const refs = {
-  input: document.querySelector('#name-input'),
-  span: document.querySelector('#name - ouput'),
-};
-
-refs.input.addEventListener('input', onInputChange);
+input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-  refs.span.textContent = event.target.value;
-  if (refs.input.value === '') {
+  span.textContent = event.currentTarget.value;
+  if (input.value === '') {
     span.textContent = 'Anonymous';
   }
 }
+
+// const refs = {
+//   input: document.querySelector('#name-input'),
+//   span: document.querySelector('#name - ouput'),
+// };
+
+// refs.input.addEventListener('input', onInputChange);
+
+// function onInputChange(event) {
+//   refs.span.textContent = event.target.value;
+//   if (event.target.value === '') {
+//     refs.span.textContent = 'Anonymous';
+//   }
+// }
